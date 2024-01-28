0d4b6cfb2af17c2868c0aca56a97ae5a87da9ece
Sean-IT <128379384+SeanIT05@users.noreply.github.com>
2024-01-28 19:14:24 +0000

    uname -r (kernel version display)

--- .config/slstatus/config.h
+++ .config/slstatus/config.h
@@ -66,8 +66,9 @@ static const struct arg args[] = {
 	//{ datetime, "%s",           "%F %T" },
 	//{ wifi_perc, "W: (%3s%% on ", "enp6s0" },
 	{ run_command, ": %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
-	{ netspeed_rx, "[󱎔 %s%%] ", "enp6s0" },
 	{ cpu_perc, "[CPU  %s%%]   ", NULL	      },
 	{ ram_perc, "[RAM  %s%%]   ", NULL	      },
-	{ datetime, "%s",           "%a %b %d %r" },
+  { kernel_release, "[ %s] ", NULL       },
+  { datetime, "%s",           "%a %b %d %r" },
 };
+
