cdea9940fee38b73f218d46b22c9cb71041b6295
Sean-IT <128379384+SeanIT05@users.noreply.github.com>
2024-01-29 14:44:13 +0000

    Monokai dwm theme

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -1,27 +1,29 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel size of windows */
+static const unsigned int borderpx  = 5;        /* border pixel size of windows */
 static const unsigned int gappx     = 5;        /* gaps size between windows */
+static const unsigned int gappih    = 30;
+static const unsigned int gappiv    = 30;
+static const unsigned int gappoh    = 30;
+static const unsigned int gappov    = 30;
 static const unsigned int snap      = 32;       /* snap pixel */
+static                int smartgaps = 0;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrains Mono NL:pixelsize=14:antialias=true:autohint=true" };
 //static const char dmenufont[]       = "JetBrains Mono NL:pixelsize=14:antialias=true:autohint=true";
 //background color
-static const char col_gray1[]       = "#222222";
-//inactive window border color
-static const char col_gray2[]       = "#444444";
-//font color
-static const char col_gray3[]       = "#d8e2ed";
-//current tag and current window font color
-static const char col_gray4[]       = "#d8e2ed";
-//Top bar second color (blue) and active window border color
-static const char col_cyan[]        = "#757a80";
+static const char col_gray1[]       = "#202a36";
+static const char col_gray2[]       = "#644475";
+static const char col_gray3[]       = "#f8f8f2";
+static const char col_gray4[]       = "#202a36";
+static const char col_gray5[]       = "#FFD866";
+static const char col_cyan[]        = "#bf93f9";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray5  },
 };
 
 /* tagging */
@@ -35,7 +37,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "LibreWolf",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
