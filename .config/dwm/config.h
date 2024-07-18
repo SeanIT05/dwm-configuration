0cf40ad5a242955df695b2a81c1a8368fefaa7b5
Sean-IT <128379384+SeanIT05@users.noreply.github.com>
2024-07-18 02:09:21 +0200

    Adjusted rofi

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *dmenucmd[] = { "rofi", "-theme", "dracula", "-show", "drun", "-icon-theme", "Papirus", "-show-icons", NULL };
+static const char *dmenucmd[] = { "rofi", "-theme", "tokyonight", "-show", "drun", "-icon-theme", "Papirus", "-show-icons", NULL };
 static const char *monitor[] = { "/usr/bin/btop", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *upvol[]   = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
