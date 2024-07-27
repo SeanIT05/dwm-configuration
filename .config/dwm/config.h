d8362a7a394a7c866b1c37452f81f82bc291d764
Sean-IT <128379384+SeanIT05@users.noreply.github.com>
2024-07-27 16:39:21 +0200

    Changed font to terminus

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -11,7 +11,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static                int smartgaps = 0;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono NL:pixelsize=14:antialias=true:autohint=true" };
+static const char *fonts[]          = { "terminus:pixelsize=14:antialias=true:autohint=true" };
 //static const char dmenufont[]       = "JetBrains Mono NL:pixelsize=14:antialias=true:autohint=true";
 //background color
 static const char col_gray1[]       = "#222222";
